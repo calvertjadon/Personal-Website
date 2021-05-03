a045c8bd4e645f6fb1cd40bc2ab4e8a07bcb0c2c
Jadon Calvert <calvertjadon@gmail.com>
2021-05-03 14:59:36 -0400

    first commit

---
+++ posts/first-post.md
@@ -0,0 +1,11 @@
+---
+title: First Post
+summary: My First Post
+author: Jadon Calvert
+date: 03 May 2021
+---
+
+* this
+* is
+* a
+* post
---
+++ src/routes/index.svelte
@@ -0,0 +1,20 @@
+<script>
+  // import the list of posts
+  import { posts } from "../posts";
+</script>
+
+<svelte:head>
+  <title>Sapper project template</title>
+</svelte:head>
+
+<h1>My Blog</h1>
+
+<!-- iterate through each post -->
+{#each posts as post}
+  <article>
+    <!-- link article to /posts/$permalink -->
+    <a href={`/posts/${post.permalink}`}>
+      <h2>{post.title}</h2>
+    </a>
+  </article>
+{/each}
